46bafb9114214967164d862fd4a6592f3dbb6a52
ybgbob <bob.g@nodereal.io>
2022-07-15 19:11:05 +0800

    chore: Typescirpt defined file

--- src/api.d.ts
+++ src/api.d.ts
@@ -1,30 +1,47 @@
 declare namespace API {
+  interface IReqBaseParam {
+    offset: number;
+    limit: number;
+  }
+
   const API_MAP: {
-    GetTxsByPubKey: 'GET /api/v1/tx/getTxsByPubKey',
-    GetTxsByAccountName: 'GET /api/v1/tx/getTxsByAccountName',
-    GetTxsByAccountIndexAndTxType: 'GET /api/v1/tx/getTxsByAccountIndexAndTxType',
-    GetTxsListByAccountIndex: 'GET /api/v1/tx/getTxsListByAccountIndex',
-    Search: 'GET /api/v1/info/search',
-    GetAccounts: 'GET /api/v1/info/getAccounts',
-    GetGasFeeAssetList: 'GET /api/v1/info/getGasFeeAssetList',
-    GetWithdrawGasFee: 'GET /api/v1/info/getWithdrawGasFee',
-    GetGasFee: 'GET /api/v1/info/getGasFee',
-    GetAssetsList: 'GET /api/v1/info/getAssetsList',
-    GetLayer2BasicInfo: 'GET /api/v1/info/getLayer2BasicInfo',
-    GetBlockByCommitment: 'GET /api/v1/block/getBlockByCommitment',
-    GetBalanceByAssetIdAndAccountName:
-      'GET /api/v1/account/getBalanceByAssetIdAndAccountName',
-    GetAccountStatusByAccountName:
-      'GET /api/v1/account/getAccountStatusByAccountName',
-    GetAccountInfoByAccountIndex:
-      'GET /api/v1/account/getAccountInfoByAccountIndex',
-    GetAccountInfoByPubKey: 'GET /api/v1/account/getAccountInfoByPubKey',
-    GetAccountStatusByAccountPk:
-      'GET /api/v1/account/getAccountStatusByAccountPk',
-    GetCurrencyPriceBySymbol: 'GET /api/v1/info/getCurrencyPriceBySymbol',
-    GetCurrencyPrices: 'GET /api/v1/info/getCurrencyPrices',
-    GetSwapAmount: 'GET /api/v1/pair/getSwapAmount',
-    GetAvailablePairs: 'GET /api/v1/pair/getAvailablePairs'
+    GetTxsByPubKey: 'GET /api/v1/tx/getTxsByPubKey';
+    GetTxsByAccountName: 'GET /api/v1/tx/getTxsByAccountName';
+    GetTxsByAccountIndexAndTxType: 'GET /api/v1/tx/getTxsByAccountIndexAndTxType';
+    GetTxsListByAccountIndex: 'GET /api/v1/tx/getTxsListByAccountIndex';
+    Search: 'GET /api/v1/info/search';
+    GetAccounts: 'GET /api/v1/info/getAccounts';
+    GetGasFeeAssetList: 'GET /api/v1/info/getGasFeeAssetList';
+    GetWithdrawGasFee: 'GET /api/v1/info/getWithdrawGasFee';
+    GetGasFee: 'GET /api/v1/info/getGasFee';
+    GetAssetsList: 'GET /api/v1/info/getAssetsList';
+    GetLayer2BasicInfo: 'GET /api/v1/info/getLayer2BasicInfo';
+    GetBlockByCommitment: 'GET /api/v1/block/getBlockByCommitment';
+    GetBalanceByAssetIdAndAccountName: 'GET /api/v1/account/getBalanceByAssetIdAndAccountName';
+    GetAccountStatusByAccountName: 'GET /api/v1/account/getAccountStatusByAccountName';
+    GetAccountInfoByAccountIndex: 'GET /api/v1/account/getAccountInfoByAccountIndex';
+    GetAccountInfoByPubKey: 'GET /api/v1/account/getAccountInfoByPubKey';
+    GetAccountStatusByAccountPk: 'GET /api/v1/account/getAccountStatusByAccountPk';
+    GetCurrencyPriceBySymbol: 'GET /api/v1/info/getCurrencyPriceBySymbol';
+    GetCurrencyPrices: 'GET /api/v1/info/getCurrencyPrices';
+    GetSwapAmount: 'GET /api/v1/pair/getSwapAmount';
+    GetAvailablePairs: 'GET /api/v1/pair/getAvailablePairs';
+    GetLPValue: 'GET /api/v1/pair/getLPValue';
+    GetPairInfo: 'GET /api/v1/pair/getPairInfo';
+    GetTxByHash: 'GET /api/v1/tx/getTxByHash';
+    GetMempoolTxs: 'GET /api/v1/tx/getMempoolTxs';
+    GetMempoolTxsByAccountName: 'GET /api/v1/tx/getmempoolTxsByAccountName';
+    GetAccountInfoByAccountName: 'GET /api/v1/account/getAccountInfoByAccountName';
+    GetNextNonce: 'GET /api/v1/tx/getNextNonce';
+    GetTxsListByBlockHeight: 'GET /api/v1/tx/getTxsListByBlockHeight';
+    GetMaxOfferId: 'GET /api/v1/nft/getMaxOfferId';
+    GetBlockByHeight: 'GET /api/v1/block/getBlockByBlockHeight';
+    GetBlocks: 'GET /api/v1/block/getBlocks';
+    SendRawTx: 'POST /api/v1/tx/sendTx';
+    SendRawCreateCollectionTx: 'POST /api/v1/tx/sendCreateCollectionTx';
+    SendRawMintNftTx: 'POST /api/v1/tx/sendMintNftTx';
+    MintNft: 'POST /api/v1/tx/sendMintNftTx';
+    CreateCollection: 'POST /api/v1/tx/sendCreateCollectionTx';
   };
 
   // 'GetTxsByPubKey' | 'GetTxsByAccountName' | ... | 'GetAvailablePairs'
@@ -34,24 +51,149 @@ declare namespace API {
   type URL_INFO = typeof API_MAP[API_NAME];
 
   interface IReqParmsMap {
-    [API_MAP.GetTxsByPubKey]: Zk.ReqParam.IGetTxsByPubKeyParam;
-    [API_MAP.GetTxsByAccountName]: Zk.ReqParam.IGetTxsByAccountNameParam;
-    [API_MAP.GetTxsByAccountIndexAndTxType]: Zk.ReqParam.IGetTxsByAccountIndexAndTxTypeParam;
-    [API_MAP.GetTxsListByAccountIndex]: Zk.ReqParam.IGetTxsListByAccountIndexParam;
-    [API_MAP.Search]: Zk.ReqParam.ISearchParam;
-    [API_MAP.GetAccounts]: Zk.ReqParam.IReqBaseParam;
-    // [API_MAP.GetGasFeeAssetList]: {};
+    [API_MAP.GetTxsByPubKey]: IReqBaseParam & {
+      account_pk: Zk.PublicKey;
+    };
+    [API_MAP.GetTxsByAccountName]: IReqBaseParam & {
+      account_name: Zk.AccountName;
+    };
+    [API_MAP.GetTxsByAccountIndexAndTxType]: IReqBaseParam & {
+      account_index: Zk.AccountIndex;
+      tx_type: number;
+    };
+    [API_MAP.GetTxsListByAccountIndex]: IReqBaseParam & {
+      account_index: Zk.AccountIndex;
+    };
+    [API_MAP.Search]: { info: string };
+    [API_MAP.GetAccounts]: IReqBaseParam;
+    [API_MAP.GetGasFeeAssetList]: {};
+    [API_MAP.GetWithdrawGasFee]: {
+      asset_id: Zk.AssetId;
+      withdraw_asset_id: Zk.AssetId;
+      withdraw_amount: Zk.Amount;
+    };
+    [API_MAP.GetGasFee]: { assetId: Zk.AssetId };
+    [API_MAP.GetAssetsList]: {};
+    [API_MAP.GetLayer2BasicInfo]: {};
+    [API_MAP.GetBlockByCommitment]: { blockCommitment: string };
+    [API_MAP.GetAccountStatusByAccountName]: { account_name: Zk.AccountName };
+    [API_MAP.GetAccountInfoByAccountIndex]: { account_index: Zk.AccountIndex };
+    [API_MAP.GetAccountInfoByPubKey]: { account_pk: Zk.AccountPk };
+    [API_MAP.GetAccountStatusByAccountPk]: { account_pk: Zk.AccountPk };
+    [API_MAP.GetCurrencyPriceBySymbol]: { symbol: string };
+    [API_MAP.GetCurrencyPrices]: {};
+    [API_MAP.GetSwapAmount]: {
+      pair: number;
+      asset_id: Zk.AssetId;
+      asset_amount: Zk.Amount;
+      is_from: boolean;
+    };
+    [API_MAP.GetAvailablePairs]: {};
+    [API_MAP.GetLPValue]: {
+      pair_index: Zk.Pair['pair_index'];
+      lp_amount: Zk.Amount;
+    };
+    [API_MAP.GetPairInfo]: { pair_index: Zk.Pair['pair_index'] };
+    [API_MAP.GetTxByHash]: { tx_hash: Zk.Hash };
+    [API_MAP.GetMempoolTxs]: IReqBaseParam;
+    [API_MAP.GetMempoolTxsByAccountName]: { account_name: Zk.AccountName };
+    [API_MAP.GetAccountInfoByAccountName]: { account_name: Zk.AccountName };
+    [API_MAP.GetNextNonce]: { account_index: Zk.AccountIndex };
+    [API_MAP.GetTxsListByBlockHeight]: IReqBaseParam & {
+      block_height: number;
+    };
+    [API_MAP.GetMaxOfferId]: { account_index: Zk.AccountIndex };
+    [API_MAP.GetBlockByHeight]: { block_height: number };
+    [API_MAP.GetBlocks]: IReqBaseParam;
+    [API_MAP.SendRawTx]: { tx_type: number; tx_info: string };
+    [API_MAP.SendRawCreateCollectionTx]: { tx_info: string };
+    [API_MAP.SendRawMintNftTx]: { tx_info: string };
+    [API_MAP.MintNft]: { tx_info: string };
+    [API_MAP.CreateCollection]: { tx_info: string };
   }
 
   interface IResponseMap {
-    [API_MAP.GetTxsByPubKey]: Zk.Response.IGetTxsRes;
-    [API_MAP.GetTxsByAccountName]: Zk.Response.IGetTxsRes;
-    [API_MAP.GetTxsByAccountIndexAndTxType]: Zk.Response.IGetTxsRes;
-    [API_MAP.GetTxsListByAccountIndex]: Zk.Response.IGetTxsRes;
-    [API_MAP.Search]: Zk.Response.ISearchRes;
-    [API_MAP.GetAccounts]: Zk.Response.IGetAccountsRes;    
+    [API_MAP.GetTxsByPubKey]: { total: number; txs: Zk.Tx[] };
+    [API_MAP.GetTxsByAccountName]: { total: number; txs: Zk.Tx[] };
+    [API_MAP.GetTxsByAccountIndexAndTxType]: { total: number; txs: Zk.Tx[] };
+    [API_MAP.GetTxsListByAccountIndex]: { total: number; txs: Zk.Tx[] };
+    [API_MAP.Search]: { data_type: string };
+    [API_MAP.GetAccounts]: {
+      total: number;
+      accounts: Zk.Account & { public_key: Zk.PublicKey }[];
+    };
+    [API_MAP.GetGasFeeAssetList]: {};
+    [API_MAP.GetWithdrawGasFee]: { gas_fee: number };
+    [API_MAP.GetGasFee]: { gas_fee: number };
+    [API_MAP.GetAssetsList]: { assets: Zk.AssetInfo[] };
+    [API_MAP.GetLayer2BasicInfo]: Zk.Layer2BasicInfo;
+    [API_MAP.GetBlockByCommitment]: { block: string };
+    [API_MAP.GetAccountStatusByAccountName]: {
+      account_status: Zk.AccountStatus;
+      account_index: Zk.AccountIndex;
+      account_pk: Zk.AccountPk;
+    };
+    [API_MAP.GetAccountInfoByAccountIndex]: {
+      account_status: Zk.AccountStatus;
+      account_name: Zk.AccountName;
+      account_pk: Zk.AccountPk;
+      assets: Zk.AccountAsset[];
+    };
+    [API_MAP.GetAccountInfoByPubKey]: {
+      account_status: Zk.AccountStatus;
+      account_name: Zk.AccountName;
+      account_index: Zk.AccountIndex;
+      assets: Zk.AccountAsset[];
+    };
+    [API_MAP.GetAccountStatusByAccountPk]: {
+      account_status: Zk.AccountStatus;
+      account_name: Zk.AccountName;
+      account_index: Zk.AccountIndex;
+    };
+    [API_MAP.GetCurrencyPriceBySymbol]: {
+      assetId: Zk.AssetId;
+      price: Zk.Price;
+    };
+    [API_MAP.GetCurrencyPrices]: {
+      data: { pair: string; assetId: Zk.AssetId; price: Zk.Price };
+    };
+    [API_MAP.GetSwapAmount]: {
+      res_asset_amount: Zk.Amount;
+      res_asset_id: Zk.AssetId;
+    };
+    [API_MAP.GetAvailablePairs]: { result: Zk.Pair };
+    [API_MAP.GetLPValue]: Zk.LP;
+    [API_MAP.GetPairInfo]: Pick<
+      Zk.Pair,
+      'asset_a_id' | 'asset_a_amount' | 'asset_b_id' | 'asset_b_amount'
+    > & { total_lp_amount: Zk.Amount };
+    [API_MAP.GetTxByHash]: {
+      result: Zk.Tx;
+      committed_at: Zk.TimeStamp;
+      verified_at: Zk.TimeStamp;
+      executed_at: Zk.TimeStamp;
+      asset_a_id: Zk.AssetId;
+      asset_b_id: Zk.AssetId;
+    };
+    [API_MAP.GetMempoolTxs]: { total: number; mempool_txs: Zk.Tx[] };
+    [API_MAP.GetMempoolTxsByAccountName]: {
+      total: number;
+      mempool_txs: Zk.Tx[];
+    };
+    [API_MAP.GetAccountInfoByAccountName]: Zk.Account & {
+      nonce: number;
+      account_pk: Zk.PublicKey;
+      assets: Zk.AccountAsset[];
+    };
+    [API_MAP.GetNextNonce]: { nonce: Zk.Nonce };
+    [API_MAP.GetTxsListByBlockHeight]: Zk.Tx[];
+    [API_MAP.GetMaxOfferId]: { offset_id: number };
+    [API_MAP.GetBlockByHeight]: { block: Zk.Block };
+    [API_MAP.GetBlocks]: { total: number; block: Zk.Block[] };
+    [API_MAP.SendRawTx]: { tx_id: string };
+    [API_MAP.SendRawCreateCollectionTx]: { collection_id: number };
+    [API_MAP.SendRawMintNftTx]: { nft_index: number };
+    [API_MAP.MintNft]: { nft_index: number };
+    [API_MAP.CreateCollection]: { collection_id: number };
   }
-
-
-
 }
--- src/zk.d.ts
+++ src/zk.d.ts
@@ -1,11 +1,33 @@
 declare namespace Zk {
+  type Hash = string;
+
+  type Fee = string;
+
+  type Nonce = number;
+
+  type TimeStamp = number;
+
+  type Block = {
+    block_commitment: string;
+    block_height: number;
+    state_root: string;
+    priority_operations: number;
+    pending_on_chain_operations_hash: Hash;
+    pending_on_chain_operations_hub_data: string;
+    committed_tx_hash: Hash;
+    committed_at: TimeStamp;
+    verified_tx_hash: Hash;
+    verified_at: TimeStamp;
+    txs: Tx[];
+    block_status: number;
+  };
+
   type AssetId = number;
   type AssetName = string;
-  type AssetDecimals = {};
-  type AssetSymbol = {};
-  type AssetAddress = {};
-  type IsGasAsset = {};
-
+  type AssetDecimals = number;
+  type AssetSymbol = string;
+  type AssetAddress = string;
+  type IsGasAsset = boolean;
 
   type AssetInfo = {
     asset_id: AssetId;
@@ -14,133 +36,95 @@ declare namespace Zk {
     asset_symbol: AssetSymbol;
     asset_address: AssetAddress;
     is_gas_asset: IsGasAsset;
-  }
-  
+  };
+
   type Price = number;
   type Amount = string;
 
   type AccountIndex = number;
   type AccountName = string;
+  type AccountStatus = number;
   type PublicKey = string;
+  type AccountPk = PublicKey;
+
+  type AccountAsset = {
+    asset_id: AssetId;
+    balance: string;
+    lp_amount: Amount;
+    offer_canceled_or_finalized: string;
+  };
 
   type Account = {
-    account_index: AccountIndex,
+    account_index: AccountIndex;
     account_name: AccountName;
-    public_key: PublicKey;
-  }
+  };
 
-  namespace ReqParam {
-    interface IReqBaseParam {
-      offset: number;
-      limit: number;
-    }
+  type Layer2BasicInfo = {
+    block_committed: number;
+    block_verified: number;
+    total_transactions: number;
+    transactions_count_yesterday: number;
+    transactions_count_today: number;
+    dau_yesterday: number;
+    dau_today: number;
+    contract_addresses: number;
+  };
 
-    interface IGetTxsByPubKeyParam extends IReqBaseParam {
-      account_pk: string;
-    }
-  
-    interface IGetTxsByAccountNameParam extends IReqBaseParam {
-      account_name: AccountName;
-    }
+  type Pair = {
+    pair_index: number;
+    asset_a_id: AssetId;
+    asset_a_name: string;
+    asset_a_amount: Amount;
+    asset_b_id: AssetId;
+    asset_b_name: string;
+    asset_b_amount: Amount;
+    fee_Rate: number;
+    treasury_rate: number;
+  };
 
-    interface IGetTxsByAccountIndexAndTxTypeParam extends IReqBaseParam {
-      account_index: AccountIndex;
-      tx_type: number;
-    }
+  type LP = {
+    asset_a_id: AssetId;
+    asset_a_name: string;
+    asset_a_amount: Amount;
+    asset_b_id: AssetId;
+    asset_b_name: string;
+    asset_b_amount: Amount;
+  };
 
-    interface IGetTxsListByAccountIndexParam extends IReqBaseParam {
-      account_index: AccountIndex;
-    }
+  type TxDetail = {
+    tx_id: number;
+    asset_id: AssetId;
+    asset_type: number;
+    account_index: AccountIndex;
+    account_name: AccountName;
+    balance: string;
+    balance_delta: string;
+    order: number;
+    account_order: number;
+    nonce: Nonce;
+    collection_nonce: Nonce;
+  };
 
-    interface ISearchParam {
-      info: string;
-    }
-  }
-
-  namespace Response {
-    interface IGetTxsRes {
-      total: number;
-      txs?: Tx[];
-    }
-
-    interface ISearchRes {
-      data_type: string;
-    }
-
-    interface IGetAccountsRes {
-      total: number;
-      accounts: Account[]
-    }
-
-    interface IGetGasFeeAssetList {
-      assets: AssetInfo[];
-    }
-
-
-    type RespGetCurrencyPriceBySymbol = {
-      assetId: AssetId;
-      price: Price;
-    };
-  
-    type DataCurrencyPrices = {
-      pair: string;
-      assetId: AssetId;
-      price: Price;
-    };
-  
-    type RespGetSwapAmount = {
-      res_asset_amount: string;
-      res_asset_id: AssetId;
-    };
-  
-    type Pair = {
-      pair_index: number;
-      asset_a_id: number;
-      asset_a_name: string;
-      asset_a_amount: Amount;
-      asset_b_id: number;
-      asset_b_name: string;
-      asset_b_amount: Amount;
-      fee_Rate: number;
-      treasury_rate: number;
-    };
-  
-    type TxDetail = {
-      tx_id: number;
-      asset_id: number;
-      asset_type: number;
-      account_index: number;
-      account_name: string;
-      balance: string;
-      balance_delta: string;
-      order: number;
-      account_order: number;
-      nonce: number;
-      collection_nonce: number;
-    };
-  
-    type Tx = {
-      tx_hash: string;
-      tx_type: number;
-      gas_fee: string;
-      gas_fee_asset_id: number;
-      tx_status: number;
-      block_height: number;
-      block_id: number;
-      state_root: string;
-      nft_index: number;
-      pair_index: number;
-      asset_id: number;
-      tx_amount: string;
-      native_address: string;
-      tx_info: string;
-      tx_details: TxDetail[];
-      extra_info: string;
-      memo: string;
-      account_index: number;
-      nonce: number;
-      expired_at: number;
-    };
-
-  }
+  type Tx = {
+    tx_hash: Hash;
+    tx_type: number;
+    gas_fee: Fee;
+    gas_fee_asset_id: AssetId;
+    tx_status: number;
+    block_height: number;
+    block_id: number;
+    state_root: string;
+    nft_index: number;
+    pair_index: number;
+    asset_id: number;
+    tx_amount: string;
+    native_address: string;
+    tx_info: string;
+    tx_details: TxDetail[];
+    extra_info: string;
+    memo: string;
+    account_index: AccountIndex;
+    nonce: Nonce;
+    expired_at: TimeStamp;
+  };
 }
