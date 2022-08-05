247743c1e539c4e4ab2abfaa411056dbade190ae
rr523 <617273330@qq.com>
2022-08-05 15:30:21 +0800

    build: Update Workflow

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['master'],
+  branches: ['master', 'release'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
