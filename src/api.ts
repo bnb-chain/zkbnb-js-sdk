69d76b5ccec4b26e00aea59d5a7bc6dec72bb847
rr523 <617273330@qq.com>
2022-08-30 19:31:00 +0800

    refactor!: New API
    BREAKING CHANGE: Modify many API

--- src/api.ts
+++ src/api.ts
@@ -6,46 +6,40 @@ export interface IReqBaseParam {
 }
 
 export const API_MAP = {
-  GetTxsByPubKey: 'GET /api/v1/tx/getTxsByPubKey',
-  GetTxsByAccountName: 'GET /api/v1/tx/getTxsByAccountName',
-  GetTxsByAccountIndexAndTxType: 'GET /api/v1/tx/getTxsByAccountIndexAndTxType',
-  GetTxsListByAccountIndex: 'GET /api/v1/tx/getTxsListByAccountIndex',
-  Search: 'GET /api/v1/info/search',
-  GetAccounts: 'GET /api/v1/info/getAccounts',
-  GetGasFeeAssetList: 'GET /api/v1/info/getGasFeeAssetList',
-  GetWithdrawGasFee: 'GET /api/v1/info/getWithdrawGasFee',
-  GetGasFee: 'GET /api/v1/info/getGasFee',
-  GetAssetsList: 'GET /api/v1/info/getAssetsList',
-  GetLayer2BasicInfo: 'GET /api/v1/info/getLayer2BasicInfo',
-  GetBlockByCommitment: 'GET /api/v1/block/getBlockByCommitment',
-  GetBalanceByAssetIdAndAccountName: 'GET /api/v1/account/getBalanceByAssetIdAndAccountName',
-  GetAccountStatusByAccountName: 'GET /api/v1/account/getAccountStatusByAccountName',
-  GetAccountInfoByAccountIndex: 'GET /api/v1/account/getAccountInfoByAccountIndex',
-  GetAccountInfoByPubKey: 'GET /api/v1/account/getAccountInfoByPubKey',
-  GetAccountStatusByAccountPk: 'GET /api/v1/account/getAccountStatusByAccountPk',
-  GetCurrencyPriceBySymbol: 'GET /api/v1/info/getCurrencyPriceBySymbol',
-  GetCurrencyPrices: 'GET /api/v1/info/getCurrencyPrices',
-  GetSwapAmount: 'GET /api/v1/pair/getSwapAmount',
-  GetAvailablePairs: 'GET /api/v1/pair/getAvailablePairs',
-  GetLPValue: 'GET /api/v1/pair/getLPValue',
-  GetPairInfo: 'GET /api/v1/pair/getPairInfo',
-  GetTxByHash: 'GET /api/v1/tx/getTxByHash',
-  GetMempoolTxs: 'GET /api/v1/tx/getMempoolTxs',
-  GetMempoolTxsByAccountName: 'GET /api/v1/tx/getmempoolTxsByAccountName',
-  GetAccountInfoByAccountName: 'GET /api/v1/account/getAccountInfoByAccountName',
-  GetNextNonce: 'GET /api/v1/tx/getNextNonce',
-  GetTxsListByBlockHeight: 'GET /api/v1/tx/getTxsListByBlockHeight',
-  GetMaxOfferId: 'GET /api/v1/nft/getMaxOfferId',
-  GetBlockByHeight: 'GET /api/v1/block/getBlockByBlockHeight',
-  GetBlocks: 'GET /api/v1/block/getBlocks',
-  SendRawTx: 'POST /api/v1/tx/sendTx',
+  GetTx: 'GET /api/v1/tx',
+  GetTxs: 'GET /api/v1/txs',
+  GetTxsByAccount: 'GET /api/v1/accountTxs',
+  Search: 'GET /api/v1/search',
+  GetAccounts: 'GET /api/v1/accounts',
+  GetGasFeeAssets: 'GET /api/v1/gasFeeAssets',
+  GetWithdrawGasFee: 'GET /api/v1/withdrawGasFee',
+  GetGasFee: 'GET /api/v1/gasFee',
+  GetAssets: 'GET /api/v1/assets',
+  GetLayer2BasicInfo: 'GET /api/v1/layer2BasicInfo',
+
+  GetBlockByParam: 'GET /api/v1/block',
+
+  GetAccountByParam: 'GET /api/v1/account',
+  GetCurrencyPrice: 'GET /api/v1/currencyPrice',
+  GetCurrencyPrices: 'GET /api/v1/currencyPrices',
+  GetSwapAmount: 'GET /api/v1/swapAmount',
+  GetPair: 'GET /api/v1/pair',
+  GetPairs: 'GET /api/v1/pairs',
+  GetLPValue: 'GET /api/v1/lpValue',
+  GetMempoolTxs: 'GET /api/v1/mempoolTxs',
+  GetMempoolTxsByAccountName: 'GET /api/v1/accountMempoolTxs',
+  GetNextNonce: 'GET /api/v1/nextNonce',
+  GetTxsByBlockHeight: 'GET /api/v1/blockTxs',
+  GetMaxOfferId: 'GET /api/v1/maxOfferId',
+  GetBlocks: 'GET /api/v1/blocks',
+  SendRawTx: 'POST /api/v1/sendTx',
   SendRawCreateCollectionTx: 'POST /api/v1/tx/sendCreateCollectionTx',
   SendRawMintNftTx: 'POST /api/v1/tx/sendMintNftTx',
+
   MintNft: 'POST /api/v1/tx/sendMintNftTx',
   CreateCollection: 'POST /api/v1/tx/sendCreateCollectionTx',
-  GetGasAccount: 'GET /api/v1/info/getGasAccount',
-  GetAccountNftList: 'GET /api/v1/nft/getAccountNftList',
-  GetTxsList: 'GET /api/v1/tx/getTxsList',
+  GetGasAccount: 'GET /api/v1/gasAccount',
+  GetNftsByAccountIndex: 'GET /api/v1/accountNfts',
 } as const;
 
 // 'GetTxsByPubKey' | 'GetTxsByAccountName' | ... | 'CreateCollection'
@@ -54,169 +48,135 @@ export type API_NAME = keyof typeof API_MAP;
 // 'GET /api/v1/tx/getTxsByPubKey' | ... | 'POST /api/v1/tx/sendCreateCollectionTx'
 export type URL_INFO = typeof API_MAP[API_NAME];
 
+interface AccountPK {
+  value: Zk.PublicKey;
+  by: 'account_pk';
+}
+
+interface AccountName {
+  value: Zk.AccountName;
+  by: 'account_name';
+}
+
+interface AccountIndex {
+  value: Zk.AccountIndex;
+  by: 'account_index';
+}
+
+export type CurrencyPrice = {
+  pair: string;
+  asset_id: Zk.Asset['id'];
+  price: string;
+};
+
 export interface IReqParmsMap {
-  [API_MAP.GetTxsByPubKey]: IReqBaseParam & {
-    account_pk: Zk.PublicKey;
-  };
-  [API_MAP.GetTxsByAccountName]: IReqBaseParam & {
-    account_name: Zk.AccountName;
-  };
-  [API_MAP.GetTxsByAccountIndexAndTxType]: IReqBaseParam & {
-    account_index: Zk.AccountIndex;
-    tx_type: number;
-  };
-  [API_MAP.GetTxsListByAccountIndex]: IReqBaseParam & {
-    account_index: Zk.AccountIndex;
-  };
-  [API_MAP.Search]: { info: string };
+  [API_MAP.GetTxsByAccount]: IReqBaseParam & (AccountPK | AccountName | AccountIndex);
+  [API_MAP.GetTxs]: IReqBaseParam;
+  [API_MAP.Search]: { keyword: string };
   [API_MAP.GetAccounts]: IReqBaseParam;
-  [API_MAP.GetGasFeeAssetList]: Record<string, never>;
+  [API_MAP.GetGasFeeAssets]: Record<string, never>;
   [API_MAP.GetWithdrawGasFee]: {
-    asset_id: Zk.AssetId;
-    withdraw_asset_id: Zk.AssetId;
+    asset_id: Zk.Asset['id'];
+    withdraw_asset_id: Zk.Asset['id'];
     withdraw_amount: Zk.Amount;
   };
-  [API_MAP.GetGasFee]: { asset_id: Zk.AssetId };
-  [API_MAP.GetAssetsList]: Record<string, never>;
+  [API_MAP.GetGasFee]: { asset_id: Zk.Asset['id'] };
+  [API_MAP.GetAssets]: IReqBaseParam;
   [API_MAP.GetLayer2BasicInfo]: Record<string, never>;
-  [API_MAP.GetBlockByCommitment]: { block_commitment: string };
-  [API_MAP.GetBalanceByAssetIdAndAccountName]: {
-    asset_id: Zk.AssetId;
-    account_name: Zk.AccountName;
-  };
-  [API_MAP.GetAccountStatusByAccountName]: { account_name: Zk.AccountName };
-  [API_MAP.GetAccountInfoByAccountIndex]: { account_index: Zk.AccountIndex };
-  [API_MAP.GetAccountInfoByPubKey]: { account_pk: Zk.AccountPk };
-  [API_MAP.GetAccountStatusByAccountPk]: { account_pk: Zk.AccountPk };
-  [API_MAP.GetCurrencyPriceBySymbol]: { symbol: string };
-  [API_MAP.GetCurrencyPrices]: Record<string, never>;
+  [API_MAP.GetBlockByParam]: { by: 'commitment'; value: string } | { by: 'height'; value: number };
+
+  [API_MAP.GetAccountByParam]:
+    | { by: 'index'; value: Zk.AccountIndex }
+    | { by: 'pk'; value: Zk.PublicKey }
+    | { by: 'name'; value: Zk.AccountName };
+
+  [API_MAP.GetCurrencyPrice]: { symbol: string };
+  [API_MAP.GetCurrencyPrices]: IReqBaseParam;
   [API_MAP.GetSwapAmount]: {
     pair_index: number;
-    asset_id: Zk.AssetId;
-    asset_amount: Zk.Amount;
+    asset_id: Zk.Asset['id'];
+    asset_amount: string;
     is_from: boolean;
   };
-  [API_MAP.GetAvailablePairs]: Record<string, never>;
+  [API_MAP.GetPairs]: IReqBaseParam;
   [API_MAP.GetLPValue]: {
-    pair_index: Zk.Pair['pair_index'];
+    pair_index: Zk.Pair['index'];
     lp_amount: Zk.Amount;
   };
-  [API_MAP.GetPairInfo]: { pair_index: Zk.Pair['pair_index'] };
-  [API_MAP.GetTxByHash]: { tx_hash: Zk.Hash };
+  [API_MAP.GetPair]: { index: Zk.Pair['index'] };
+  [API_MAP.GetTx]: { hash: Zk.Hash };
   [API_MAP.GetMempoolTxs]: IReqBaseParam;
-  [API_MAP.GetMempoolTxsByAccountName]: { account_name: Zk.AccountName };
-  [API_MAP.GetAccountInfoByAccountName]: { account_name: Zk.AccountName };
+  [API_MAP.GetMempoolTxsByAccountName]: { by: 'account_name'; value: Zk.AccountName };
+
   [API_MAP.GetNextNonce]: { account_index: Zk.AccountIndex };
-  [API_MAP.GetTxsListByBlockHeight]: IReqBaseParam & {
-    block_height: number;
+  [API_MAP.GetTxsByBlockHeight]: {
+    by: 'block_height';
+    value: number;
   };
   [API_MAP.GetMaxOfferId]: { account_index: Zk.AccountIndex };
-  [API_MAP.GetBlockByHeight]: { block_height: number };
   [API_MAP.GetBlocks]: IReqBaseParam;
-  [API_MAP.SendRawTx]: { tx_type: number; tx_info: string };
+  [API_MAP.SendRawTx]: { tx_type: string; tx_info: string };
   [API_MAP.SendRawCreateCollectionTx]: { tx_info: string };
   [API_MAP.SendRawMintNftTx]: { tx_info: string };
   [API_MAP.MintNft]: { tx_info: string };
   [API_MAP.CreateCollection]: { tx_info: string };
   [API_MAP.GetGasAccount]: Record<string, never>;
-  [API_MAP.GetAccountNftList]: IReqBaseParam & { account_index: Zk.AccountIndex };
-  [API_MAP.GetTxsList]: IReqBaseParam;
+  [API_MAP.GetNftsByAccountIndex]: IReqBaseParam & { by: 'account_index'; value: Zk.AccountIndex };
 }
 
 export interface IResponseMap {
-  [API_MAP.GetTxsByPubKey]: { total: number; txs: Zk.Tx[] };
-  [API_MAP.GetTxsByAccountName]: { total: number; txs: Zk.Tx[] };
-  [API_MAP.GetTxsByAccountIndexAndTxType]: { total: number; txs: Zk.Tx[] };
-  [API_MAP.GetTxsListByAccountIndex]: { total: number; txs: Zk.Tx[] };
-  [API_MAP.Search]: { data_type: string };
+  [API_MAP.GetTxsByAccount]: { total: number; txs: Zk.Tx[] };
+  [API_MAP.GetTxs]: { total: number; txs: Zk.Tx[] };
+  [API_MAP.Search]: { data_type: number };
   [API_MAP.GetAccounts]: {
     total: number;
-    accounts: (Zk.Account & { public_key: Zk.PublicKey })[];
+    accounts: Zk.SimpleAccount[];
   };
-  [API_MAP.GetGasFeeAssetList]: Record<string, never>;
-  [API_MAP.GetWithdrawGasFee]: { gas_fee: number };
-  [API_MAP.GetGasFee]: { gas_fee: number };
-  [API_MAP.GetAssetsList]: { assets: Zk.AssetInfo[] };
+  [API_MAP.GetGasFeeAssets]: { assets: Zk.Asset[] };
+  [API_MAP.GetWithdrawGasFee]: { gas_fee: string };
+  [API_MAP.GetGasFee]: { gas_fee: string };
+  [API_MAP.GetAssets]: { total: number; assets: Zk.Asset[] };
   [API_MAP.GetLayer2BasicInfo]: Zk.Layer2BasicInfo;
-  [API_MAP.GetBlockByCommitment]: { block: string };
-  [API_MAP.GetBalanceByAssetIdAndAccountName]: { balance_enc: string };
-  [API_MAP.GetAccountStatusByAccountName]: {
-    account_status: Zk.AccountStatus;
-    account_index: Zk.AccountIndex;
-    account_pk: Zk.AccountPk;
-  };
-  [API_MAP.GetAccountInfoByAccountIndex]: {
-    account_status: Zk.AccountStatus;
-    account_name: Zk.AccountName;
-    account_pk: Zk.AccountPk;
-    assets: Zk.AccountAsset[];
-  };
-  [API_MAP.GetAccountInfoByPubKey]: {
-    account_status: Zk.AccountStatus;
-    account_name: Zk.AccountName;
-    account_index: Zk.AccountIndex;
-    assets: Zk.AccountAsset[];
-  };
-  [API_MAP.GetAccountStatusByAccountPk]: {
-    account_status: Zk.AccountStatus;
-    account_name: Zk.AccountName;
-    account_index: Zk.AccountIndex;
-  };
-  [API_MAP.GetCurrencyPriceBySymbol]: {
-    assetId: Zk.AssetId;
-    price: Zk.Price;
-  };
+  [API_MAP.GetBlockByParam]: Zk.Block;
+  [API_MAP.GetBlocks]: { total: number; blocks: Zk.Block[] };
+  [API_MAP.GetAccountByParam]: Zk.Account;
+  [API_MAP.GetCurrencyPrice]: CurrencyPrice;
   [API_MAP.GetCurrencyPrices]: {
-    data: { pair: string; assetId: Zk.AssetId; price: Zk.Price };
+    total: number;
+    currency_prices: CurrencyPrice[];
   };
   [API_MAP.GetSwapAmount]: {
-    res_asset_amount: Zk.Amount;
-    res_asset_id: Zk.AssetId;
+    asset_id: Zk.Asset['id'];
+    asset_name: Zk.Asset['name'];
+    asset_amount: string;
   };
-  [API_MAP.GetAvailablePairs]: { result: Zk.Pair };
+  [API_MAP.GetPairs]: { pairs: Zk.Pair[] };
   [API_MAP.GetLPValue]: Zk.LP;
-  [API_MAP.GetPairInfo]: Pick<Zk.Pair, 'asset_a_id' | 'asset_a_amount' | 'asset_b_id' | 'asset_b_amount'> & {
-    total_lp_amount: Zk.Amount;
-  };
-  [API_MAP.GetTxByHash]: {
-    result: Zk.Tx;
+  [API_MAP.GetPair]: Zk.Pair;
+  [API_MAP.GetTx]: Zk.Tx & {
     committed_at: Zk.TimeStamp;
     verified_at: Zk.TimeStamp;
     executed_at: Zk.TimeStamp;
-    asset_a_id: Zk.AssetId;
-    asset_b_id: Zk.AssetId;
+    asset_a_id: Zk.Asset['id'];
+    asset_b_id: Zk.Asset['id'];
   };
-  [API_MAP.GetMempoolTxs]: { total: number; mempool_txs: Zk.Tx[] };
+  [API_MAP.GetMempoolTxs]: { total: number; txs: Zk.Tx[] };
   [API_MAP.GetMempoolTxsByAccountName]: {
     total: number;
     mempool_txs: Zk.Tx[];
   };
-  [API_MAP.GetAccountInfoByAccountName]: Zk.Account & {
-    nonce: number;
-    account_pk: Zk.PublicKey;
-    assets: Zk.AccountAsset[];
-  };
+
   [API_MAP.GetNextNonce]: { nonce: Zk.Nonce };
-  [API_MAP.GetTxsListByBlockHeight]: Zk.Tx[];
-  [API_MAP.GetMaxOfferId]: { offset_id: number };
-  [API_MAP.GetBlockByHeight]: { block: Zk.Block };
-  [API_MAP.GetBlocks]: { total: number; blocks: Zk.Block[] };
+  [API_MAP.GetTxsByBlockHeight]: { txs: Zk.Tx[]; total: number };
+  [API_MAP.GetMaxOfferId]: { offer_id: number };
   [API_MAP.SendRawTx]: { tx_id: string };
   [API_MAP.SendRawCreateCollectionTx]: { collection_id: number };
   [API_MAP.SendRawMintNftTx]: { nft_index: number };
   [API_MAP.MintNft]: { nft_index: number };
   [API_MAP.CreateCollection]: { collection_id: number };
-  [API_MAP.GetGasAccount]: {
-    account_status: Zk.AccountStatus;
-    account_index: Zk.AccountIndex;
-    account_name: Zk.AccountName;
-  };
-  [API_MAP.GetAccountNftList]: {
+  [API_MAP.GetGasAccount]: Zk.GasAccount;
+  [API_MAP.GetNftsByAccountIndex]: {
     total: number;
     nfts: Zk.Nft[];
-  };
-  [API_MAP.GetTxsList]: {
-    total: number;
-    txs: Zk.Tx[];
   };
 }
