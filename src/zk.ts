bfd39fa75d57929ffe38c11858df453a1f90ec40
rr523 <617273330@qq.com>
2022-08-30 18:49:29 +0800

    refactor: BREAKING CHANGE New API

--- src/zk.ts
+++ src/zk.ts
@@ -7,34 +7,27 @@ export type Nonce = number;
 export type TimeStamp = number;
 
 export type Block = {
-  block_commitment: string;
-  block_height: number;
+  commitment: string;
+  height: number;
   state_root: string;
   priority_operations: number;
   pending_on_chain_operations_hash: Hash;
-  pending_on_chain_operations_hub_data: string;
+  pending_on_chain_operations_pub_data: string;
   committed_tx_hash: Hash;
   committed_at: TimeStamp;
   verified_tx_hash: Hash;
   verified_at: TimeStamp;
   txs: Tx[];
-  block_status: number;
+  status: number;
 };
 
-export type AssetId = number;
-export type AssetName = string;
-export type AssetDecimals = number;
-export type AssetSymbol = string;
-export type AssetAddress = string;
-export type IsGasAsset = boolean;
-
-export type AssetInfo = {
-  asset_id: AssetId;
-  asset_name: AssetName;
-  asset_decimals: AssetDecimals;
-  asset_symbol: AssetSymbol;
-  asset_address: AssetAddress;
-  is_gas_asset: IsGasAsset;
+export type Asset = {
+  id: number;
+  name: string;
+  decimals: number;
+  symbol: string;
+  address: string;
+  is_gas_asset: number;
 };
 
 export type Price = number;
@@ -47,52 +40,74 @@ export type PublicKey = string;
 export type AccountPk = PublicKey;
 
 export type AccountAsset = {
-  asset_id: AssetId;
+  id: number;
+  name: string;
   balance: string;
-  lp_amount: Amount;
-  offer_canceled_or_finalized: string;
+  lp_amount: string;
+};
+
+export type GasAccount = {
+  staus: number;
+  index: number;
+  name: string;
 };
 
 export type Account = {
-  account_index: AccountIndex;
-  account_name: AccountName;
+  status: number;
+  index: number;
+  name: string;
+  pk: string;
+  nonce: number;
+  assets: AccountAsset[];
+};
+
+export type SimpleAccount = {
+  index: AccountIndex;
+  name: AccountName;
+  pk: AccountPk;
+};
+
+type ContractAddress = {
+  name: string;
+  address: string;
 };
 
 export type Layer2BasicInfo = {
   block_committed: number;
   block_verified: number;
-  total_transactions: number;
-  transactions_count_yesterday: number;
-  transactions_count_today: number;
-  dau_yesterday: number;
-  dau_today: number;
-  contract_addresses: number;
+  total_transaction_count: number;
+  yesterday_transaction_count: number;
+  today_transaction_count: number;
+  yesterday_active_user_count: number;
+  today_active_user_count: number;
+  contract_addresses: ContractAddress[];
 };
 
 export type Pair = {
-  pair_index: number;
-  asset_a_id: AssetId;
+  index: number;
+  asset_a_id: Asset['id'];
   asset_a_name: string;
   asset_a_amount: Amount;
-  asset_b_id: AssetId;
+  asset_b_id: Asset['id'];
   asset_b_name: string;
   asset_b_amount: Amount;
-  fee_Rate: number;
+  fee_rate: number;
   treasury_rate: number;
+  total_lp_amount: string;
 };
 
 export type LP = {
-  asset_a_id: AssetId;
+  asset_a_id: Asset['id'];
   asset_a_name: string;
   asset_a_amount: Amount;
-  asset_b_id: AssetId;
+  asset_b_id: Asset['id'];
   asset_b_name: string;
   asset_b_amount: Amount;
 };
 
 export type TxDetail = {
   tx_id: number;
-  asset_id: AssetId;
+  asset_id: Asset['id'];
   asset_type: number;
   account_index: AccountIndex;
   account_name: AccountName;
@@ -128,37 +143,39 @@ export const TxType = {
 } as const;
 
 export type Tx = {
-  account_index: AccountIndex;
+  hash: Hash;
+  type: number;
+  amount: string;
+  info: string;
+  status: number;
+  index: number;
+  gas_fee_asset_id: Asset['id'];
+  gas_fee: Fee;
+  nft_index: number;
+  collection_id: number;
+  pair_index: number;
   asset_id: number;
+  asset_name: string;
+  native_address: string;
+  extra_info: string;
+  memo: string;
+  account_index: AccountIndex;
+  account_name: string;
+  nonce: Nonce;
+  expired_at: TimeStamp;
   block_height: number;
   block_id: number;
   create_at: TimeStamp;
-  expired_at: TimeStamp;
-  extra_info: string;
-  gas_fee_asset_id: AssetId;
-  gas_fee: Fee;
-  memo: string;
-  native_address: string;
-  nft_index: number;
-  nonce: Nonce;
-  pair_index: number;
   state_root: string;
-  status: number;
-  tx_amount: string;
-  tx_details: TxDetail[];
-  tx_hash: Hash;
-  tx_info: string;
-  tx_status: number;
-  tx_type: number;
 };
 
 export type Nft = {
-  nft_index: number;
+  index: number;
   creator_account_index: number;
   owner_account_index: number;
-  nft_content_hash: Hash;
-  nft_l1_address: string;
-  nft_l1_token_id: string;
+  content_hash: Hash;
+  l1_address: string;
+  l1_token_id: string;
   creator_treasury_rate: number;
   collection_id: number;
 };
